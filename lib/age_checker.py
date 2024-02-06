f32b03fbe7233ec8e408d1098e003c1b3161386c
Alex Bull <alex.r.bull505@outlook.com>
2024-02-06 16:07:57 +0000

    Complete

--- lib/age_checker.py
+++ lib/age_checker.py
@@ -1,8 +1,13 @@
 from datetime import *
 from dateutil.relativedelta import *
-import re 
 
 def age_checker(dob):
+    if type(dob) != str:
+        raise Exception("String not entered: Please enter a valid iterable")
+    try:
+        date.fromisoformat(dob)
+    except ValueError:
+        raise ValueError("String contians invalid formatting: Please enter a string in the format YYYY-MM-DD")
     dob_datetime = datetime.strptime(dob,"%Y-%m-%d")
     today = date.today()
     age_from_dob = relativedelta(today,dob_datetime).years
@@ -10,4 +15,3 @@ def age_checker(dob):
         return f"Access Denied: You are {age_from_dob} and the required age is 16 or over!"
     else:
         return "Access Granted!"
-
--- tests/test_age_checker.py
+++ tests/test_age_checker.py
@@ -1,4 +1,5 @@
 from lib.age_checker import *
+import pytest
 
 """
 Given a valid DOB over the age of 16 
@@ -13,3 +14,25 @@ Return "Access Denied: You are 3 and the required age is 16 or over!"
 """
 def test_access_denied_under_16():
     assert age_checker("2020-06-15") == "Access Denied: You are 3 and the required age is 16 or over!"
+
+
+"""
+Given an invalid type 
+Return "String not entered"
+"""
+
+def test_exception_invalid_type():
+    with pytest.raises(Exception) as e:
+        age_checker(15)
+    assert str(e.value) == "String not entered: Please enter a valid iterable"
+
+
+"""
+Given an invalid date format 
+Return "Invalid date format entered"
+"""
+
+def test_date_formatting_exception():
+    with pytest.raises(Exception) as e:
+        age_checker("2020.06.15")
+    assert str(e.value) == "String contians invalid formatting: Please enter a string in the format YYYY-MM-DD"
